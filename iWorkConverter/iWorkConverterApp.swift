1789a3f78783fe7ae925e8bb0512273589201396
acer51-doctom <110639772+acer51-doctom@users.noreply.github.com>
2025-03-05 14:31:02 +0100

    Initial Commit

---
+++ iWorkConverter/iWorkConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWorkConverterApp.swift
+//  iWorkConverter
+//
+//  Created by Arthur on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct iWorkConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iWorkConverterTests/iWorkConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  iWorkConverterTests.swift
+//  iWorkConverterTests
+//
+//  Created by Arthur on 05/03/2025.
+//
+
+import Testing
+@testable import iWorkConverter
+
+struct iWorkConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
